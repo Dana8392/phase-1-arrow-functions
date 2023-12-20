9432674e494ff6268dc4476fb10c7e9ce35ede62
Dana Maraqa <dana.maraqa@hotmail.com>
2023-12-20 19:25:44 +0300

    Reviewed

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 // Function expression called divide
-const divide = function(a, b) {
-    return a / b;
-  };
-  
-  // Arrow function called square
-  const square = (x) => {
-    return x * x;
-  };
-  
-  // Arrow function called add
-  const add = (a, b) => {
-    return a + b;
-  };
-  
+const divide = function (a,b) {
+  return a/b;
+};
+
+
+// Arrow function called square
+const square = x => x * x ; 
+
+// Arrow function called add
+const add = (a,b) => {
+  return a + b
+};
